13ec1f3742e502c478867c025c7bb70c980eee09
Thomas Wessel <turpleturtle12@gmail.com>
2024-11-07 11:09:22 -0800

    Modify vite config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,12 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: process.env.REPO_NAME || "/repo-name/",
   plugins: [
     /**
      * We're using `SharedArrayBuffer`s in our code,
      * which requires either HTTPS or localhost, and it requires cross origin isolation.
      * So we're enabling the CORS headers here for development mode.
-     *
-     * Do note that your production server will need HTTPS and CORS headers set up correctly.
-     * If you cannot control the HTTP headers that your production server sends back (like on GitHub pages),
-     * then there's a workaround using a service worker. See https://dev.to/stefnotch/enabling-coop-coep-without-touching-the-server-2d3n
      */
     {
       // Plugin code is from https://github.com/chaosprint/vite-plugin-cross-origin-isolation
